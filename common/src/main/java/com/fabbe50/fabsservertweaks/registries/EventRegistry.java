3f86dbd17b6d967e94739b404df24f740ef3d14f
William <fabbe50@users.noreply.github.com>
2025-09-27 14:05:26 +0200

    Mob Tool Drop on Death Gamerule

--- common/src/main/java/com/fabbe50/fabsservertweaks/registries/EventRegistry.java
+++ common/src/main/java/com/fabbe50/fabsservertweaks/registries/EventRegistry.java
@@ -15,6 +15,25 @@ import net.minecraft.world.level.Level;
 
 public class EventRegistry {
     public static void init() {
+        EntityEvent.LIVING_DEATH.register((livingEntity, damageSource) -> {
+            if (livingEntity instanceof Mob mob) {
+                Level level = mob.level();
+                if (level instanceof ServerLevel serverLevel) {
+                    GameRules gameRules = serverLevel.getGameRules();
+                    if (!gameRules.getBoolean(ModGameRules.RULE_MOB_DROP_EQUIPABLE)) {
+                        for (EquipmentSlot slot : EquipmentSlot.values()) {
+                            if (!mob.getDropChances().isPreserved(slot)) {
+                                ItemStack stack = mob.getItemBySlot(slot);
+                                if (stack.has(DataComponents.EQUIPPABLE) || stack.has(DataComponents.TOOL) || stack.has(DataComponents.WEAPON) || stack.getItem() instanceof ProjectileWeaponItem) {
+                                    stack.setCount(0);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return EventResult.pass();
+        });
         CommandRegistrationEvent.EVENT.register((commandDispatcher, commandBuildContext, commandSelection) -> {
             GotoCommand.register(commandDispatcher);
         });
