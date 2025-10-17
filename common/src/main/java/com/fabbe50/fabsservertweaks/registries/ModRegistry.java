d5fd878e7bcd4c6ec4a1abf97f53ae7035284974
William <fabbe50@users.noreply.github.com>
2025-10-17 23:50:38 +0200

    Tag Key for potion blacklist

--- common/src/main/java/com/fabbe50/fabsservertweaks/registries/ModRegistry.java
+++ common/src/main/java/com/fabbe50/fabsservertweaks/registries/ModRegistry.java
@@ -1,7 +1,12 @@
 package com.fabbe50.fabsservertweaks.registries;
 
-public class ModRegistry {
+import com.fabbe50.fabsservertweaks.Fabsservertweaks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
 
+public class ModRegistry {
+    public static TagKey<EntityType<?>> MOBS_WITH_POTION_EFFECTS_BLACKLIST = TagKey.create(Registries.ENTITY_TYPE, Fabsservertweaks.location("mobs_with_potion_effects_blacklist"));
 
     public static void init() {}
 }
