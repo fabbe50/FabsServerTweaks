0b43fe99f8eb1076a1173e64a597d4e69a9743d9
William <fabbe50@gmail.com>
2025-10-29 16:17:12 +0100

    Gamerules for dripstone filling cauldrons

--- common/src/main/java/com/fabbe50/fabsservertweaks/registries/ModGameRules.java
+++ common/src/main/java/com/fabbe50/fabsservertweaks/registries/ModGameRules.java
@@ -28,6 +28,8 @@ public class ModGameRules {
     public static final GameRules.Key<BooleanValue> RULE_NO_COBBLE_GEN;
     public static final GameRules.Key<BooleanValue> RULE_SHARE_SEED;
     public static final GameRules.Key<BooleanValue> RULE_BETTER_HOES;
+    public static final GameRules.Key<BooleanValue> RULE_WATER_DRIPSTONE_FILL_CAULDRON;
+    public static final GameRules.Key<BooleanValue> RULE_LAVA_DRIPSTONE_FILL_CAULDRON;
 
     static {
         RULE_MOB_GRIEF_ENDERMAN = GameRules.register("mobGriefEnderman", Category.MOBS, BooleanValue.create(true));
@@ -48,6 +50,8 @@ public class ModGameRules {
         RULE_NO_COBBLE_GEN = GameRules.register("noCobbleGen", Category.MISC, BooleanValue.create(false));
         RULE_SHARE_SEED = GameRules.register("shareSeed", Category.MISC, BooleanValue.create(false));
         RULE_BETTER_HOES = GameRules.register("betterHoes", Category.MISC, BooleanValue.create(true));
+        RULE_WATER_DRIPSTONE_FILL_CAULDRON = GameRules.register("dripstoneFillCauldronWater", Category.MISC, BooleanValue.create(true));
+        RULE_LAVA_DRIPSTONE_FILL_CAULDRON = GameRules.register("dripstoneFillCauldronLava", Category.MISC, BooleanValue.create(true));
     }
 
     public static void init() {}
