e134f683fb62fceb09fd2c53571fd3ade9c4391e
William <fabbe50@users.noreply.github.com>
2026-07-02 21:45:42 +0200

    Removed Client Events from NeoForge as they are no longer needed

--- neoforge/src/main/java/com/fabbe50/fabsservertweaks/neoforge/FabsservertweaksClientNeoForge.java
+++ neoforge/src/main/java/com/fabbe50/fabsservertweaks/neoforge/FabsservertweaksClientNeoForge.java
@@ -1,13 +1,11 @@
 package com.fabbe50.fabsservertweaks.neoforge;
 
 import com.fabbe50.fabsservertweaks.Fabsservertweaks;
-import com.fabbe50.fabsservertweaks.neoforge.event.ClientEvents;
 import com.fabbe50.fabsservertweaks.neoforge.plugins.ClothConfigPlugin;
 import dev.architectury.platform.Platform;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(value = Fabsservertweaks.MOD_ID, dist = Dist.CLIENT)
 public final class FabsservertweaksClientNeoForge {
@@ -17,7 +15,5 @@ public final class FabsservertweaksClientNeoForge {
         if (Platform.isModLoaded("cloth_config")) {
             ClothConfigPlugin.registerConfigScreen(container);
         }
-
-        NeoForge.EVENT_BUS.register(new ClientEvents());
     }
 }
